457d9c4c57abdd2614e226d9ed3f7cf743425205
nicoman <nicolasmanso@gmail.com>
2015-02-10 14:06:38 -0300

    Add new requirements to the app

--- superlists/functional_tests/test.py
+++ superlists/functional_tests/test.py
@@ -41,9 +41,11 @@ class NewVisitorTest(LiveServerTestCase):
         # fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list
+        # When she hits enter, she is taken to a new URL, and now the page
+        # lists "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She enters
@@ -55,6 +57,19 @@ class NewVisitorTest(LiveServerTestCase):
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # Now a new user, Francis, comes along to the site.
+
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies etc #'
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('But peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
         # Edith wonders whether the site will remember her list. Then she sees that the
         # site has generated a unique URL for her -- there is some explanatory text
